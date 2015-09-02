17e85d1e6cdfaa4ba60ea12ee17ff3ef3a4643a3
Shivam Vats <shivamvats.iitkgp@gmail.com>
2015-09-02 15:32:35 +0530

    Add test for King class and use signum to find color

--- src/xchess/King.java
+++ src/xchess/King.java
@@ -11,12 +11,12 @@ public class King {
 		ArrayList<Point> moves = new ArrayList<>(10);
 		int x = current.getX();
 		int y = current.getY();
-		int myColor = Math.abs(Game.cb.getElement(x, y));
+		int myColor = (int)Math.signum(Game.cb.getElement(x, y));
 		
 		for(int i=x-1; i<=x+1; i++)
 			for(int j=y-1; j<=y+1; j++) {
 				if(i!=x && j!=y && i>=0 && j>=0 && i<8 && j<8) {
-					int otherColor = Math.abs(Game.cb.getElement(i, j));
+					int otherColor = (int)Math.signum(Game.cb.getElement(i, j));
 					if(otherColor != myColor)
 						moves.add(new Point(i, j));	
 				}
---
+++ src/xchess/tests/TestKing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xchess.tests;
+
+import java.util.ArrayList;
+import xchess.Game;
+import xchess.Point;
+import xchess.King;
+
+/**
+ *
+ * @author oop25
+ */
+public class TestKing {
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.main();
+		g.cb.getState();
+		
+		ArrayList<Point> m=new ArrayList<Point>();
+		King r = new King();
+		m = r.getPossibleMoves(new Point(3,4));
+		Point p = new Point(0,0);
+		for(int i=0;i<m.size();i++)
+		{
+			System.out.println((m.get(i)));
+		}
+		
+	}
+}
