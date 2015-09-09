44fdf27dc5d56bad1b4f07bccfb2b2475a0ac0c7
Shivam Vats <shivamvats.iitkgp@gmail.com>
2015-09-09 16:52:49 +0530

    Add choosePiece function

--- src/xchess/Game.java
+++ src/xchess/Game.java
@@ -1,8 +1,36 @@
 package xchess;
 
+import java.util.Scanner;
+
 public class Game {
 	public static ChessBoard cb = new ChessBoard();
+	private static int turn = 1;
+	
 	public static void main(String[] args) {
+		while(true) {
 		cb.getState();
+		Point movePosition = choosePiece();
+		//m = r.getPossibleMoves(new Point(3,4));
+		//for(int i=0;i<m.size();i++)
+		//{
+		//	System.out.println((m.get(i)));
+		//}
+		}
+		
+	}
+
+	private static Point choosePiece() {
+		while(true) {
+			System.out.println("Enter coordinates of the piece to be moved\n");
+			int x, y;
+			Scanner in = new Scanner(System.in);
+			x = in.nextInt();
+			y = in.nextInt();
+			if((x >= 0 && x < 8) && (y >= 0 && y< 8) &&(turn == (int)Math.signum(cb.getElement(x, y))))
+					return new Point(x, y);
+			else
+				System.out.println("Please enter a valid position\n");
+		}
+			
 	}
 }
